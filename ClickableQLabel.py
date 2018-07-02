d237894d75839415c7ee50244e98da48e981c482
Archisman Dey <archismandey13@gmail.com>
2018-07-03 00:57:29 +0530

    added sensible import statements

--- ClickableQLabel.py
+++ ClickableQLabel.py
@@ -1,12 +1,11 @@
 #implement a clickable QLabel used for showing the circle and crosses as a QPixelMap
 
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
-from PyQt5.QtWidgets import *
+import PyQt5.QtCore
+from PyQt5.QtWidgets import QLabel
 
 class ClickableQLabel (QLabel):
 
-	clicked = pyqtSignal()	#must be declared here, not inside init
+	clicked = PyQt5.QtCore.pyqtSignal()	#must be declared here, not inside init
 
 	def __init__ (self, parent = None):
 		super().__init__(parent)
